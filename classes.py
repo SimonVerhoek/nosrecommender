d92fb6ec61a14407c4479605717628f8879f9073
Simon Verhoek <saverhoek@gmail.com>
2015-04-03 20:34:58 +0200

    Article class now takes variable amount of inputs, and user can name keys itself on instantiation

--- classes.py
+++ classes.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
-class Article:
+class Article(dict):
 
 	articleCount = 0
 
-	def __init__(self, url, title, categories, body, image):
-		self.url = url
-		self.title = title
-		self.categories = categories
-		self.body = body
-		self.image = image
-		Article.articleCount += 1
+	def __init__(self, **kwargs):
+		for key, value in kwargs.items():
+			self.update(kwargs)
+			
+		self.articleCount += 1
 
 	def displayCount(self):
 		print "Number of articles is %d" % Article.articleCount
