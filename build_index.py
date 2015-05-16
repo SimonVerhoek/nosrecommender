1542eae2ca18d36e3c198c1149b58f162c107fbc
Simon Verhoek <saverhoek+x@gmail.com>
2015-05-16 21:36:36 +0200

    stripped build_index from scraping functions

--- build_index.py
+++ build_index.py
@@ -2,10 +2,7 @@
 
 import os.path
 import json
-from datetime import datetime, date, timedelta
 from sys import exit
-from bs4 import BeautifulSoup
-from urllib2 import urlopen
 
 from classes import Article, Collection
 
@@ -17,40 +14,21 @@ fileName = "archive.json"
 # local file, give correct filepath here
 localArchive =  "/Users/simonverhoek/Google Drive/Studie/Web search/Project/nosrecommender/" + fileName
 
-# If you want to create an index without a
-# local file, set the name for the index here:
-# WARNING:  name should be lowercase and without
-#           spaces!
-archiveName = "nosarchive"
-
-# set number of days back in time to
-# be scraped. If set to 1, only today's
-# archive is scraped.
-noDays = 1
-
 
 def main():
-    print
-    print "===== STEP 1: BUILDING A NEWS ARCHIVE ====="
-    print
-    """ 
-    OPTION 1: Import an existing archive.
     """
-    #archive = get_existing_archive(fileName)
-        
-    """ 
-    OPTION 2: Build a new archive.
+    Imports and builds an archive of news articles.
     """
-    archive = build_new_archive(archiveName, noDays)
 
-    print
-    print "===== STEP 2: INDEXING THE NEWS ARCHIVE ====="
-    print
-    
+    # import an existing archive.
+    archive = import_archive(fileName)
+
+    # build ElasticSearch index of archive
     Collection.build_index(archive)
     Collection.index_articles(archive)
 
-def get_existing_archive(fileName):
+
+def import_archive(fileName):
     """
     Imports an existing archive from a .json file.
     Exits when file is not found.
@@ -79,66 +57,8 @@ def import_collection(localFile):
 
     for k, v in content.iteritems():
         print "Imported: %s, %d articles" % (k, len(v))
+        print
         return k, v
-
-
-def build_new_archive(archiveName, noDays):
-    """
-    Builds new archive by scraping the given news website.
-    The procedure is as follows: 
-    1:  Scrape urls for a set number of days. 
-    2:  Instantiate Collection.
-    3:  For each url, instantiate Article. The article's
-        contents are scraped automatically by the Article 
-        class.
-    4:  Add article to collection instance.
-    """
-    urls = scrape_urls(noDays, date)
-    noArticles = len(urls)
-
-    archive = Collection(archiveName)
-
-    for articleNo, url in enumerate(urls):
-        print "Processing article %d of %d..." % (articleNo+1, noArticles)
-        article = Article(url)
-        archive.add_article(article)
-
-    Collection.display_articleCount(archive)
-    return archive
-
-def scrape_urls(noDays, date):
-    """ 
-    Scrapes the NOS "archief" page for article urls
-    for a set amount of days. Returns a list named 
-    "urls" containing all found article urls.
-    -   noDays should be an integer, ranging from 1.
-    -   date should be datetime.today().
-    """
-    date = datetime.today()
-    urls = []
-
-    for i in xrange(0, noDays):
-
-        # properly format date
-        year = str(date.year)
-        month = "%02d" % date.month
-        day = "%02d" % date.day
-
-        page = "http://nos.nl/nieuws/archief/" + year + "-" + month + "-" + day
-
-        # get all links from this day's webpage
-        soup = BeautifulSoup(urlopen(page))
-        for link in soup.find_all("a", {"class":"link-block"}):
-            # filter out non-article links
-            if link["href"][1:8] == "artikel":
-                link = "http://nos.nl" + link["href"]
-                urls.append(link)
-
-        # go one day back in time
-        date = date - timedelta(days=1)
-
-    print str(len(urls)) + " urls scraped from past " + str(noDays) + " days."
-    return urls
 
 
 # execute main
