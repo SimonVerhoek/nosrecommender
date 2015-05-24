b3583f29db277b21e770fc88008a4eb3e1aa3f21
Simon Verhoek <saverhoek+x@gmail.com>
2015-05-24 11:50:01 +0200

    index_archive.py now makes use of import_from_json method

--- index_archive.py
+++ index_archive.py
@@ -16,38 +16,13 @@ def main():
     """
 
     # import an existing archive.
-    archive = import_archive(archiveName)
+    #archive = import_archive(archiveName)
+    archive = Collection(archiveName)
+    archive.import_from_json(archiveName)
 
     # build ElasticSearch index of archive
     Collection.build_index(archive)
     Collection.index_articles(archive)
-
-
-def import_archive(fileName):
-    """
-    Imports an existing news archive.
-    -   fileName should be the name of a local JSON file, 
-        with an ElasticSearch-friendly format, in the 
-        "/files" subfolder. 
-    """
-    fileName = archiveName + ".json"
-
-    print 'Attempting to import from "files/' + fileName + '"...'
-
-    chdir("files")
-
-    if path.isfile(fileName):
-        content = json.loads(open(fileName, "rb").read())
-
-        for k, v in content.iteritems():
-            print "Imported: %s, %d articles" % (k, len(v))
-            print
-
-        archive = Collection(content.keys()[0], content.values()[0])
-        return archive
-    else:
-        print "File not found."
-        exit()
 
 # execute main
 if __name__ == "__main__":
