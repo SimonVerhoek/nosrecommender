957ff9bf0bbad2b4e99a5056a26479f85ddb56df
Simon Verhoek <saverhoek+po@gmail.com>
2015-05-29 14:39:11 +0200

    query_index.py now imports json with read articles

--- query_index.py
+++ query_index.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+
+from classes import Article, Collection
+
+readArticlesFileName = "read_articles"
+
+def main():
+	"""
+	Imports a collection of read articles and queries this
+	against an archive of news articles.
+	"""
+	readArticles = Collection(readArticlesFileName)
+
+	readArticles.import_from_json(readArticlesFileName)
+	
+
+# execute main
+if __name__ == "__main__":
+    main()
