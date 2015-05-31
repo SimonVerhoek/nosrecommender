348fe4d4dc6093a393ad235827ddbb22fbbc0a2c
Simon Verhoek <saverhoek+po@gmail.com>
2015-05-31 18:40:45 +0200

    added comments to query_index function

--- functions.py
+++ functions.py
@@ -12,7 +12,14 @@ from settings.index_settings import setting, mapping
 connection = ES('localhost:9200')
 
 def query_index(query, index):
-
+	"""
+	Queries index for recommended news articles.
+	Returns Collection dict with recommended articles, together
+	with their scores.
+	-	query should be a Query dict.
+	- 	index should be a string with the name of the ElasticSearch
+		index to be queried. 
+	"""
 	# create new dict for storing recommended articles in
 	recommendedArticles = Collection("recommendedArticles")
 
